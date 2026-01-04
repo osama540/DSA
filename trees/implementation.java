96fa455be2c9862ab518959187044e13e451903a
osama540 <osamaalam9508@gmail.com>
2026-01-04 14:45:17 +0530

    trees

---
+++ trees/implementation.java
@@ -0,0 +1,36 @@
+
+public class implementation {
+    public static class Node{
+        int val; // by default initialized to 0
+        Node left; // null
+        Node right; // null
+
+        public Node(int val){
+            this.val = val;
+        }
+    } 
+    public static void display(Node root){
+        if(root == null) return;
+        System.out.print(root.val+" -> ");
+        if(root.left != null) System.out.print(root.left.val+" ");
+        if(root.right != null ) System.out.print(root.right.val);
+        System.out.println();
+        display(root.left);
+        display(root.right);
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        root.left = a;
+        root.right =b;
+        Node c = new Node(4);
+        Node d = new Node(5);
+        a.left =c;
+        a.right = d;
+        Node e = new Node(6);
+        b.right = e;
+        display(root);
+    }
+    
+}
---
+++ trees/sizeoftree.java
@@ -0,0 +1,35 @@
+
+
+public class sizeoftree {
+    public static class Node{
+        int val; // by default initialized to 0
+        Node left; // null
+        Node right; // null
+
+        public Node(int val){
+            this.val = val;
+        }
+    }
+    public static void preorder(Node root){
+        if(root == null) return;
+        System.out.print(root.val+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        root.left = a;
+        root.right =b;
+        Node c = new Node(4);
+        Node d = new Node(5);
+        a.left =c;
+        a.right = d;
+        Node e = new Node(6);
+        b.right = e;
+        preorder(root);
+       // display(root);
+    }
+    
+}
